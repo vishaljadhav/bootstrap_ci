a68ea0571be0ea6142a094a64c075aca0921a0e2
Vishal <vjvishu4u@gmail.com>
2013-09-13 00:41:33 +0530

    removed all other files

--- application/views/html_meta.php
+++
@@ -1,8 +0,0 @@
-<?php 
-if (isset($meta) && is_array($meta)) {
-        foreach ($meta as $metadata){
-            echo '<meta name="'.$metadata['name'].'" content="'.$metadata['content'].'">';
-        }
-}
-
-?>
